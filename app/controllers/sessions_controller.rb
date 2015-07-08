01b5b7564f802b454dbdfc0acfa48f4a8d2df3a8
the-barm <funfundzwanzigsten@gmail.com>
2015-07-08 20:30:55 +0000

    Finishing sign-in/sign-up exercises

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
   
   def create
     
+    
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
@@ -11,6 +12,18 @@ class SessionsController < ApplicationController
       redirect_to user
     else
             # Create an error message.
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+  
+  def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
+    else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
