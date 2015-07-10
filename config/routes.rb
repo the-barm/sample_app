49ecf8a07407aa17ad5e0a32dd5b5463c210e11f
the-barm <funfundzwanzigsten@gmail.com>
2015-07-10 13:29:37 +0000

    Add account activations

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
+
 
 
 
